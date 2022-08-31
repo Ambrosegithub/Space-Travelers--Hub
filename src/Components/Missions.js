642a5767ea38cfadbd5cdea9f5a45f155204f0b8
Isigba Ambrose <ambrisexyz@gmail.com>
2022-08-31 13:03:32 +0200

    add click event on dispatch

--- src/Components/Missions.js
+++ src/Components/Missions.js
@@ -3,7 +3,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
-import { fetchMissions } from '../redux/missionFetch';
+import Button from 'react-bootstrap/Button';
+import { fetchMissions, joinMission, leaveMission } from '../redux/missionFetch';
 
 const Missions = () => {
     // get missions data from the store
@@ -14,8 +15,9 @@ const Missions = () => {
       if (!missions.length) {
         dispatch(fetchMissions);
       }
-    },[]);
-
+    },[])
+const handleJoinMission = (id) => dispatch(joinMission(id))
+const handleLeaveMission = (id) => dispatch(leaveMission(id))
   return (
     <Container className="container-fluid">
     <Table className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
@@ -29,7 +31,7 @@ const Missions = () => {
         </thead>
         <tbody >
           {missions.map(({
-            id, name, description
+            id, name, description, reserved
           }) => (
             <tr className='wrapper' key={id}>
             <td>
@@ -38,17 +40,30 @@ const Missions = () => {
             <td>
               <p>{description}</p>
             </td>
-            
-            <td>
-            <button className="primary" type='button'>Not a member</button>
-            <button className="primary" type='button'>active member</button>
-            </td>
-           
-             <td>
-            <button className="primary" onClick={() => handleJoinMission(id)}>Join Mission</button>
-            <button className="primary" onClick={() => handleLeaveMission(id)}>Leave Mission</button>
-            </td>
-          
+            <td className="badge-cont">
+                {reserved && <span className="act-badge">Active Member</span>}
+                {!reserved && <span className="badge">NOT A MEMBER</span>}
+              </td>
+              <td className="button-cont">
+                {reserved && (
+                <Button 
+                  className="leave-btn"
+                  size="sm" 
+                  variant="outline-danger"
+                  onClick={() => handleLeaveMission(id)}>
+                  Leave Mission
+                </Button>
+                )}
+                {!reserved && (
+                <Button 
+                  className="join-btn"
+                  size="sm" 
+                  variant="outline-secondary"
+                  onClick={() => handleJoinMission(id)}>
+                  Join Mission
+                </Button>
+                )}
+              </td>
           </tr>
           ))}
         </tbody>
